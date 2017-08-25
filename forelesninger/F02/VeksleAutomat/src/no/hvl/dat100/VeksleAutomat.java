89c24b68b46e2d9b6cc0fe0d96d3be0f4f48d15f
hbs <hbs@HIB-11616.hvl.no>
2017-08-25 13:48:44 +0200

    Veksle

---
+++ forelesninger/F02/VeksleAutomat/src/no/hvl/dat100/VeksleAutomat.java
@@ -0,0 +1,43 @@
+package no.hvl.dat100;
+import static javax.swing.JOptionPane.*;
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.*;
+public class VeksleAutomat {
+/*
+ * Vekslingen er begrenset til myntene 20-kroner, 10-kroner og 1-kroner
+ * Automaten gir først tilbake så mange 20-kroner som mulig og så færrest
+ * antall 10-kroner og 1-kroner.
+ * Basert på innbetalt beløp som må være større eller lik pris. Ingen sjekk.
+ * 
+ * Vi ser på to måter å løse det på
+ */
+	public static void main(String[] args) {
+		String tallTxt = showInputDialog("Skriv inn beløp:");
+		int beloep = parseInt(tallTxt);
+		tallTxt = showInputDialog("Skriv inn pris:");
+		int pris = parseInt(tallTxt);
+		//1. metode
+		int veksel = beloep - pris;
+		veksel = 1019;
+		int ant20kr = veksel/20;
+		int ant10kr = (veksel%20)/10;
+		int ant1kr =  ((veksel%20))%10;
+		veksel = 1019;
+		String ut = veksel + " kr kan veksles i " +
+		            ant20kr + " 20-kroner og " +
+				    ant10kr + " 10-kroner og " +
+		            ant1kr + " 1-kroner";
+		showMessageDialog(null,ut);
+		
+		//2. metode
+		int antall20kr = veksel/20;
+		int resten = veksel - 20*antall20kr;
+		int antall10kr = resten/10;
+		int antall1kr = resten - 10*antall10kr;
+		String ut2 = veksel + " kr kan veksles i " +
+	            antall20kr + " 20-kroner og " +
+			    antall10kr + " 10-kroner og " +
+	            antall1kr + " 1-kroner";
+	showMessageDialog(null,ut);
+		 
+	}
