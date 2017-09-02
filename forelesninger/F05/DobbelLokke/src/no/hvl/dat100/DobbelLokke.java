1fa77abf9bc9845b0ff3f336106613a9139b31e7
hbs <hbs@HIB-11616.hvl.no>
2017-09-02 13:28:54 +0200

    DobbelLokke

---
+++ forelesninger/F05/DobbelLokke/src/no/hvl/dat100/DobbelLokke.java
@@ -0,0 +1,13 @@
+package no.hvl.dat100;
+
+public class DobbelLokke {
+
+	public static void main(String[] args) {
+		for (int i = 1; i <=3; i++){
+			for(int j = 1; j <= 4; j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
