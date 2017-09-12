b4d8d0b35be89d7ca65bfdc5b96b05c9a6ee12e7
soh <soh@HIB-10392.hib.no>
2017-09-12 09:51:00 +0200

    Oppgave, blir fylt inn i forelesing

--- forelesninger/F07/Valg/src/no/hvl/dat100/TelleStemmer.java
+++ forelesninger/F07/Valg/src/no/hvl/dat100/TelleStemmer.java
@@ -1,5 +1,67 @@
 package no.hvl.dat100;
 
+import static javax.swing.JOptionPane.*;
+
 public class TelleStemmer {
+	/*
+	 * Søk etter streng i tabell av strengar. Dersom streng finst, returner
+	 * indeks (posisjon) i tabellen. Dersom den ikkje finst, returner -1.
+	 */
+
+	// TODO
+
+	
+	/*
+	 * Har tabell av heiltal. Finn posisjon for største talet. Om er er fleire
+	 * like, bruk den første av dei
+	 */
+
+	
+	// TODO
+
+	
+	public static String lesStemme() {
+		return showInputDialog("Parti?");
+	}
+
+	public static void main(String[] args) {
+		/*
+		 * Deklarer og opprett partitabell. Ta med berre dei som ligg an til å
+		 * få mandat "Ap", "Frp", "H", "KrF", "MDG", "R", "Sp", "SV", "V"
+		 */
+		
+		
+	
+		// Deklarer tabell for å telje stemmer
+		
+
+		// Gi alle parti ei stemme.
+		
+		
+		// Gi ei stemme til MDG
+		
+		
+		// Gi to stemmer til siste partiet i tabellen
+		
+
+		// Les inn 5 stemmer og oppdater stemmetabell
+		
+
+		// Vis partiet med stemmetal som har fått flest stemmer i eit
+		// dialogvindu
+		
+		
+		
+
+		// Skriv ut stemmetabell til konsollvindu
+		/*
+		 * Ap	5
+		 * Frp	5
+		 * ...
+		 * V	5
+		 */
+		
+		
+	}
 
 }
