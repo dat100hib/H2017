b9e31feb01bda629f213f271cab1ea06332bcb2d
soh <soh@HIB-10392.hib.no>
2017-09-12 12:59:48 +0200

    Prosjektet etter forlesing 7. Resten blir fylt inn nr 8.

--- forelesninger/F07/Valg/src/no/hvl/dat100/TelleStemmer.java
+++ forelesninger/F07/Valg/src/no/hvl/dat100/TelleStemmer.java
@@ -12,10 +12,18 @@ public class TelleStemmer {
 
 	
 	/*
-	 * Har tabell av heiltal. Finn posisjon for største talet. Om er er fleire
+	 * Har tabell av heiltal. Lag ein metode for å finn posisjon for største talet. Om er er fleire
 	 * like, bruk den første av dei
 	 */
-
+	public static int maks(int[] tab) {
+		int makspos = 0;
+		for (int i = 1; i < tab.length; i++) {
+			if (tab[i] > tab[makspos]) {
+				makspos = i;
+			}
+		}
+		return makspos;
+	}
 	
 	// TODO
 
@@ -28,21 +36,25 @@ public class TelleStemmer {
 		/*
 		 * Deklarer og opprett partitabell. Ta med berre dei som ligg an til å
 		 * få mandat "Ap", "Frp", "H", "KrF", "MDG", "R", "Sp", "SV", "V"
+		 * 
 		 */
-		
-		
+		String[] partier = {"Ap", "Frp", "H", "KrF", "MDG", "R", "Sp", "SV", "V"};
+		final int ANTALL_PARTI = partier.length;
 	
 		// Deklarer tabell for å telje stemmer
-		
+		int[] stemmer = new int[ANTALL_PARTI];
 
 		// Gi alle parti ei stemme.
-		
+		for (int i = 0; i < ANTALL_PARTI; i++){
+			stemmer[i]++;
+		}
 		
 		// Gi ei stemme til MDG
-		
+		stemmer[4]++;
 		
 		// Gi to stemmer til siste partiet i tabellen
-		
+		stemmer[ANTALL_PARTI - 1] += 2;
+		// stemmer[ANTALL_PARTI - 1] = stemmer[ANTALL_PARTI - 1] + 2;
 
 		// Les inn 5 stemmer og oppdater stemmetabell
 		
@@ -50,6 +62,10 @@ public class TelleStemmer {
 		// Vis partiet med stemmetal som har fått flest stemmer i eit
 		// dialogvindu
 		
+		stemmer[4] += 3;
+		int makspos = maks(stemmer);
+		String utstreng = String.format("%s fikk flest stemmer (%d)", partier[makspos], stemmer[makspos]);
+		showMessageDialog(null, utstreng);
 		
 		
 
@@ -61,6 +77,10 @@ public class TelleStemmer {
 		 * V	5
 		 */
 		
+		for (int i = 0; i < ANTALL_PARTI; i++) {
+			String ut = String.format("%-3s%5d", partier[i], stemmer[i]);
+			System.out.println(ut);
+		}
 		
 	}
 
