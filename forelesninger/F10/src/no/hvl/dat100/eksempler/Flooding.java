4d2a81089261551e79cc182d2cc14c2ef024be08
Lars Kristensen <lmkr@hib.no>
2017-09-21 15:25:44 +0200

    Implementert bruk av Easygraphics i flooding eksempel

--- forelesninger/F10/src/no/hvl/dat100/eksempler/Flooding.java
+++ forelesninger/F10/src/no/hvl/dat100/eksempler/Flooding.java
@@ -1,17 +1,14 @@
 package no.hvl.dat100.eksempler;
 
 import easygraphics.EasyGraphics;
-import java.util.Scanner;
 
 public class Flooding extends EasyGraphics {
 
 	// nesten dekket hvis mindre en fra havoverflaten
-	int NESTEN_DEKKET_INT = 2; 
-
-	char DEKKET_CHR = '~';
-	char NESTEN_DEKKET_CHR = 'O';
-	char SIKKERT_CHR = '+';
-
+	int NESTEN_DEKKET = 1; 
+	int RADIUS = 20;
+	int MARGIN = 50;
+	
 	// terreng hogde
 	int[][] terreng = { 	{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
 						{ 0, 0, 0, 3, 3, 3, 6, 7, 8, 10 },
@@ -23,7 +20,7 @@ public class Flooding extends EasyGraphics {
 
 	public void run() {
 
-		makeWindow("FLOODING", 400,400);
+		makeWindow("FLOODING", 800,300);
 
 		visualiser(); 
 	}
@@ -36,13 +33,33 @@ public class Flooding extends EasyGraphics {
 		while (hav >= 0) {
 			
 			// TODO - START
-			
+			for (int ri = 0; ri<terreng.length; ri++) {
+				
+				for (int ki = 0; ki <terreng[ri].length; ki++) {
+					
+					int h = terreng[ri][ki];
+					
+					if (hav >= h) {
+						// under vann
+						setColor(0,0,255);
+					} else if ((h - hav) > NESTEN_DEKKET) {
+						// sikkert
+						setColor(222,184,135);
+					} else {
+						// usikkert
+						setColor(255,140,0);
+					}
+
+					fillCircle(MARGIN + ki * 2 * RADIUS,MARGIN + ri * 2* RADIUS,RADIUS);
+
+				}
+			}
 			// itererer igjennom terreng tabellen
 			// skriv rett tegn ut avhenggig av avstand fra havoverflate
 			
 			// TODO - SLUTT
-			
-			System.out.print("Hav høyde : ");
+			System.out.println("Angi havhøyde i tegnevinduet ...");
+			hav = Integer.parseInt(getText("havhøyde"));
 		}
 	}
 }
