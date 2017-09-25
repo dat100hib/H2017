0ad75e386235ef17815256fe20ca4d17895c2be0
Lars Kristensen <lmkr@hib.no>
2017-09-25 10:08:59 +0200

    Delt kode for 2-dim tabeller i to klasser

--- forelesninger/F10/src/no/hvl/dat100/eksempler/TimePlan.java
+++ forelesninger/F10/src/no/hvl/dat100/eksempler/TimePlan.java
@@ -2,10 +2,6 @@ package no.hvl.dat100.eksempler;
 
 public class TimePlan {
 
-	// datatype[][] tabellnavn = new datatype[M][N];
-	static boolean[][] btab = new boolean[4][5];
-	
-	// datatype[][] tabellnavn;
 	static String[][] timeplan = {
 			{"nor","eng","mat","nor","eng"},
 			{"gym","ofa","fri","ofa","kun"},
@@ -35,11 +31,5 @@ public class TimePlan {
 	
 	public static void main (String[] args) {
 		skrivTimeplan();
-		
-		// variable = tabellnavn[r][k]
-		boolean b = btab[1][3];
-		
-		// tabellnavn[r][k] = verdi
-		btab[1][3] = true;
 	}
 }
