ad1396586905b2244c52126c5bf4b72009096f0a
Lars Kristensen <lmkr@hib.no>
2017-10-07 00:06:06 +0200

    Lagt inn kontrollflytseksempler

--- forelesninger/F14/src/no/hvl/dat100/kontrollflytt/KontrollFlytt.java
+++ forelesninger/F14/src/no/hvl/dat100/kontrollflytt/KontrollFlytt.java
@@ -1,8 +1,15 @@
 package no.hvl.dat100.kontrollflytt;
 
+import java.io.IOException;
+import java.lang.RuntimeException;
+
 // Unntakk:
-// - 
+// - IOException
+// - RunTimeException
+
 public class KontrollFlytt {
+
+	static boolean somethingbadhappens = true;
 
 	public static void main(String[] args) {
 
@@ -21,6 +28,10 @@ public class KontrollFlytt {
 
 	public static void metode2() {
 		System.out.println("metode 2 - start");
+
+		if (somethingbadhappens) {
+			throw (new RuntimeException("Houston we have a problem!"));
+		}
 
 		System.out.println("metode 2 - slutt");
 	}
