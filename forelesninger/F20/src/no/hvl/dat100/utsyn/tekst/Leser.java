50c2994ee4dfe9d4a0081aeec1dc75c2544d7edc
Lars Kristensen <lmkr@hib.no>
2017-10-25 21:12:35 +0200

    Endret leser til å bruke next fra Scanner

--- forelesninger/F20/src/no/hvl/dat100/utsyn/tekst/Leser.java
+++ forelesninger/F20/src/no/hvl/dat100/utsyn/tekst/Leser.java
@@ -2,30 +2,29 @@ package no.hvl.dat100.utsyn.tekst;
 
 import java.util.Scanner;
 
-// hjelpeklasse for innlesning av gårds- og bruksnummer
 public class Leser {
 
 	private Scanner leser;
-	
+
 	public Leser() {
 		leser = new Scanner(System.in);
 	}
-	
+
 	public int lesInt(String tekst) {
-		
+
 		System.out.println(tekst);
-		
+
 		int tall = leser.nextInt();
-		
+
 		return tall;
 	}
-	
+
 	public String lesStreng(String tekst) {
-		
+
 		System.out.println(tekst);
-		
-		String s = leser.nextLine();
-		
+
+		String s = leser.next();
+
 		return s;
 	}
 }
