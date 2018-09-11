6582c08f1e28735a0cbe9139b64f89d1621d1632
LMK <lmkr@hvl.no>
2018-09-11 16:12:42 +0200

    Main metode F08

--- forelesninger/F08 - Programmeringsprosjekt/src/no/hvl/dat100/f08/Tabeller.java
+++ forelesninger/F08 - Programmeringsprosjekt/src/no/hvl/dat100/f08/Tabeller.java
@@ -2,9 +2,14 @@ package no.hvl.dat100.f08;
 
 public class Tabeller {
 
+	public static void skrivUt(double[] tabell ) {
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		System.out.println("Test");
+		// datatype[] tabellnavn = {v1,v2,... , vN}		
+		double[] obs = {1.1, 2.4, 6.7, 8.7, 9.0, 10.0};
 
 	}
 
