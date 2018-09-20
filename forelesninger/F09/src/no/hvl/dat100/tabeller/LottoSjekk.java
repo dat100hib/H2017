b00057d7e37d0443c0db838c161ec45a29dd12cb
Lars Michael Kristensen <lmkr@hvl.no>
2018-09-20 16:04:30 +0200

    Oppdatert Array eksempler

--- forelesninger/F09/src/no/hvl/dat100/tabeller/LottoSjekk.java
+++ forelesninger/F09/src/no/hvl/dat100/tabeller/LottoSjekk.java
@@ -9,11 +9,8 @@ public class LottoSjekk {
 	static int[] minKupong1 = {12,1,34,16,2,25,26};
 	static int[] minKupong2 = {2,7,34,16,21,1,29};
 	
-	public static boolean sjekkkupong_v1(int[] kupong, int[] lottotall) {
-		return (lottotall == kupong); 
-	}
-	
 	public static boolean finnesTall(int tall, int[] tabell) {
+		
 		boolean funnet = false;
 		int pos = 0;
 		
@@ -27,12 +24,11 @@ public class LottoSjekk {
 		return funnet;
 	}
 	
-	public static boolean sjekkkupong_v2(int[] kupong, int[] lottotall) {
+	public static boolean sjekkkupong(int[] kupong, int[] lottotall) {
 		
-		for (int i = 0; i<ANTALL; i++) {
-			int tall = lottotall[i];
+		for (int tall : kupong) {
 			
-			boolean hartall = finnesTall(tall,kupong);
+			boolean hartall = finnesTall(tall,lottotall);
 			
 			if (!hartall) {
 				return false;
@@ -44,10 +40,8 @@ public class LottoSjekk {
 	
 	public static void main(String[] args) {
 		
-		System.out.println(sjekkkupong_v1(minKupong1,lottoTall));
-		System.out.println(sjekkkupong_v1(minKupong2,lottoTall));
+		System.out.println(sjekkkupong(minKupong1,lottoTall));
+		System.out.println(sjekkkupong(minKupong2,lottoTall));
 		
-		System.out.println(sjekkkupong_v2(minKupong1,lottoTall));
-		System.out.println(sjekkkupong_v2(minKupong2,lottoTall));
 	}
 }
